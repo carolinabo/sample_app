d0896724eae068fbd74cdfd2f82269f7c83890e2
carolinabo <carolina.bormann@hotmail.de>
2013-12-18 15:32:20 +0100

    End of chapter8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -45,7 +45,9 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+        it { should have_link('Sign out') }
+
+   end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
